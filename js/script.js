92f2800901b93b7738325e526c49084597f86d9b
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-10 18:30:03 +0000

    added everything

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  // this function does basic math
+
+console.log("Hello, World!")
+  document.getElementById('multiply-math').innerHTML = '<p>The area is = ' + (5 * 3) + '</p>'
+  document.getElementById('add-math').innerHTML = '<p>7 - 3 =  ' + (7 - 3) + '</p>'
+ 
